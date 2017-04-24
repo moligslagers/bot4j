5de6daacb0258fff7ddc4627011c1c6012335011
uwol <u.wol@wwu.de>
2017-04-24 13:26:57 +0200

    restrucured speechlet

--- src/main/java/ai/nitro/bot4j/integration/alexa/impl/Bot4jSpeechletImpl.java
+++ src/main/java/ai/nitro/bot4j/integration/alexa/impl/Bot4jSpeechletImpl.java
@@ -32,11 +32,16 @@ import ai.nitro.bot4j.integration.alexa.send.AlexaMessageSender;
 import ai.nitro.bot4j.middle.domain.Participant;
 import ai.nitro.bot4j.middle.domain.receive.ReceiveMessage;
 import ai.nitro.bot4j.middle.domain.receive.nlp.NlpContext;
-import ai.nitro.bot4j.middle.domain.receive.nlp.NlpContext;
 import ai.nitro.bot4j.middle.domain.receive.payload.TextReceivePayload;
 import ai.nitro.bot4j.middle.receive.MessageReceiver;
 
 public class Bot4jSpeechletImpl implements Bot4jSpeechlet {
+
+	private static final String AMAZON_CANCEL_INTENT = "AMAZON.CancelIntent";
+
+	private static final String AMAZON_HELP_INTENT = "AMAZON.HelpIntent";
+
+	private static final String AMAZON_STOP_INTENT = "AMAZON.StopIntent";
 
 	private static final Logger LOG = LogManager.getLogger(Bot4jSpeechletImpl.class);
 
@@ -90,21 +95,69 @@ public class Bot4jSpeechletImpl implements Bot4jSpeechlet {
 		return result;
 	}
 
+	protected SpeechletResponse onCancelIntent() {
+		final PlainTextOutputSpeech speech = new PlainTextOutputSpeech();
+		speech.setText("Bye");
+
+		final SpeechletResponse result = SpeechletResponse.newTellResponse(speech);
+		result.setShouldEndSession(true);
+
+		return result;
+	}
+
+	protected SpeechletResponse onHelpIntent() {
+		final PlainTextOutputSpeech speech = new PlainTextOutputSpeech();
+		speech.setText("Ok");
+
+		final SpeechletResponse result = SpeechletResponse.newTellResponse(speech);
+		result.setShouldEndSession(false);
+
+		return result;
+	}
+
 	@Override
 	public SpeechletResponse onIntent(final SpeechletRequestEnvelope<IntentRequest> requestEnvelope) {
-		LOG.info("onIntent requestId={}, sessionId={}", requestEnvelope.getRequest().getRequestId(),
+		LOG.debug("onIntent requestId={}, sessionId={}", requestEnvelope.getRequest().getRequestId(),
 				requestEnvelope.getSession().getSessionId());
+
+		final SpeechletResponse result;
 
 		if (requestEnvelope.getRequest().getIntent() == null) {
 			LOG.warn("no intent set");
+			result = null;
 		} else if (requestEnvelope.getSession().getUser() == null) {
 			LOG.warn("no user set");
+			result = null;
 		} else {
-			receiveMessage(requestEnvelope.getRequest(), requestEnvelope.getSession().getUser());
+			final IntentRequest request = requestEnvelope.getRequest();
+			final String intentName = request.getIntent().getName();
+
+			switch (intentName) {
+			case AMAZON_HELP_INTENT:
+				result = onHelpIntent();
+				break;
+			case AMAZON_STOP_INTENT:
+				result = onStopIntent();
+				break;
+			case AMAZON_CANCEL_INTENT:
+				result = onCancelIntent();
+				break;
+			default:
+				result = onIntent(requestEnvelope, request);
+			}
 		}
 
-		final PlainTextOutputSpeech speech = new PlainTextOutputSpeech();
+		return result;
+	}
+
+	protected SpeechletResponse onIntent(final SpeechletRequestEnvelope<IntentRequest> requestEnvelope,
+			final IntentRequest request) {
+		final User user = requestEnvelope.getSession().getUser();
+
+		receiveMessage(request, user);
+
 		final String text = alexaMessageSender.getText();
+		final PlainTextOutputSpeech speech = new PlainTextOutputSpeech();
 		speech.setText(text);
 
 		final SpeechletResponse result = SpeechletResponse.newTellResponse(speech);
@@ -122,6 +175,16 @@ public class Bot4jSpeechletImpl implements Bot4jSpeechlet {
 
 	@Override
 	public void onSessionStarted(final SpeechletRequestEnvelope<SessionStartedRequest> arg0) {
+	}
+
+	protected SpeechletResponse onStopIntent() {
+		final PlainTextOutputSpeech speech = new PlainTextOutputSpeech();
+		speech.setText("Bye");
+
+		final SpeechletResponse result = SpeechletResponse.newTellResponse(speech);
+		result.setShouldEndSession(true);
+
+		return result;
 	}
 
 	protected void receiveMessage(final IntentRequest intentRequest, final User user) {
